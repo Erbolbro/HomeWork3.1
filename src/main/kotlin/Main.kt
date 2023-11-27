ebd0fe36b8ce46df91edf49e1b9ae0e407f83f3f
Erbolbro <144798174+Erbolbro@users.noreply.github.com>
2023-11-27 16:55:06 +0600

    open

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.Scanner
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val userInfo = User(username = "Beka", email = "beka@gmail.com",17,12345678)
+    val scanner=Scanner(System.`in`)
+    println("login")
+    println("Input username:")
+    val userName = scanner.nextLine()
+    println("Input email")
+    val email=scanner.nextLine()
+    println("Input Age")
+    val age = scanner.nextInt()
+    println("Input password")
+    val password = scanner.nextInt()
+
+    val inputUserModel = User(userName,email,age,password)
+    userInfo.verify(inputUserModel)
 }
--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -1,2 +1,18 @@
-class User {
-}
+   data class User(
+       var username: String,
+       var email: String,
+       var age: Int,
+       var password: Int
+   ):Verifiable{
+
+
+       override fun verify(user: User) {
+           if (this == user) {
+               println("Вход был успешно!")
+           } else{
+               throw IllegalAccessException("Неверныйе данные для входа!")
+           }
+       }
+   }
+
+
